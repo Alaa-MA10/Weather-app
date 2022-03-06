9bafec0b1a107794fabedc8315222db1de272851
Alaa MA <alaa.m.abdulaziz9@gmail.com>
2022-03-06 14:00:41 +0200

    init Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatFormFieldModule} from '@angular/material/form-field'
+import { MatSelectModule } from '@angular/material/select';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatSelectModule,
+
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
